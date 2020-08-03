8252590c4936ca820fc680b2da8c7886a0b78b55
Jose Luis Maroto Romo <jlmaroto@iesoretania.es>
2020-08-03 10:53:50 +0200

    Resuelve el programa con while

---
+++ ut02_b04_02_while.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// Algoritmo que obtenga e imprema los números pares del 2 al 24.
+// Con while
+int main(){
+    int i=2;
+
+    while(i <= 24){
+        printf("%d\n", i);
+        i = i + 2;
+    }
+return 0;
+}
